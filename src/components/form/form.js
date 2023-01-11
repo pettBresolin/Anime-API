f80b4a360406fcb616e41b1af6788e2875a49c77
Bresolin <pettbresolin@hotmail.com>
2023-01-11 04:56:31 -0300

    create anime list

---
+++ src/components/form/form.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { api } from "../../utils/api/api";
+
+export function Form({ getAll, handleModal }) {
+  const [newAnime, setNewAnime] = useState({ characters: [] });
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(event) {
+    setLoading(true);
+    event.preventDefault();
+
+    await api.createAnime(newAnime);
+    await getAll();
+    setLoading(false);
+    handleModal();
+  }
+
+  return (
+    <>
+      {loading ? (
+        <div> loading...</div>
+      ) : (
+        <div className="form">
+          <form onSubmit={handleSubmit} className="form-inputs">
+            <section>
+              <span>Title:</span>
+              <input
+                type="text"
+                name="title"
+                onChange={(event) => {
+                  setNewAnime({ ...newAnime, title: event.target.value });
+                }}
+              ></input>
+            </section>
+            <section>
+              <span>Protagonist</span>
+              <input
+                type="text"
+                name="protagonist"
+                onChange={(event) => {
+                  setNewAnime({ ...newAnime, protagonist: event.target.value });
+                }}
+              ></input>
+            </section>
+            <section>
+              <span>Gender:</span>
+              <input
+                type="text"
+                name="gender"
+                onChange={(event) => {
+                  setNewAnime({ ...newAnime, gender: event.target.value });
+                }}
+              ></input>
+            </section>
+            <section>
+              <span>Year:</span>
+              <input
+                type="number"
+                name="year"
+                onChange={(event) => {
+                  setNewAnime({ ...newAnime, year: event.target.value });
+                }}
+              ></input>
+            </section>
+            <button type="submit" className="btn-submit">
+              Submit
+            </button>
+          </form>
+        </div>
+      )}
+    </>
+  );
+}
---
+++ src/home.js
@@ -0,0 +1,12 @@
+import { Card } from "../../components/card/card";
+import { Header } from "../../header/header";
+import { api } from "../../api/api";
+import { useState, useEffect } from "react";
+import { CgClose } from "react-icons/cg";
+import Modal from "react-modal";
+import "./home.css";
+const customStyles = {};
+
+export function Home() {
+  return <div></div>;
+}
