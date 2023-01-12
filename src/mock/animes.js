223b2d6252af56dc92648111a038ea7fa3790e8d
Bresolin <pettbresolin@hotmail.com>
2023-01-11 21:54:43 -0300

    mock datas

---
+++ src/mock/animes.js
@@ -0,0 +1,9 @@
+export const animemock = [
+    {
+      title: "Bleach",
+      protagonist: "Bleach",
+      gender: "Shounem",
+      year: 2006,
+      characters: [],
+       },
+  ];
--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -1,3 +1,4 @@
+
 const defaultUrl = "https://animes-api-c017.herokuapp.com/animes";
 
 export const api = {
